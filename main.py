bca9fffccfedec461e3a891a734d5defd55c5554
edu-alasos <alansebastian.soszynski@skola.taby.se>
2025-11-06 12:44:34 +0100

    Del 2: Flera frågor och poängräkning

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+points = 0
+
 def question(fråga, options, correct):
     print(fråga)
     i = 0
@@ -8,7 +10,15 @@ def question(fråga, options, correct):
     chosen = int(input("Ditt val: "))
     if options[chosen-1] == correct:
         print("Rätt!")
+        return 1
     else:
-        print("Fel.")
-    
-question(fråga="Vilken huvudstad har Sverige?", options=["Oslo", "Stockholm", "Köpenhamn"], correct="Stockholm")
+        print(f"Fel, rätt svar var: {correct}")
+        return 0
+
+quiz = [
+    question(fråga="Vilken huvudstad har Sverige?", options=["Oslo", "Stockholm", "Köpenhamn"], correct="Stockholm"),
+    question(fråga="Vilket år började andrav ärldskriget?", options=["1914", "1939", "1945"], correct="1939")
+]
+
+print("")
+print(f"Du fick {sum(quiz)} av {len(quiz)} rätt")
